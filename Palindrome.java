d86815c7cc916ad6a29be6753dd6beb7c4b73292
ColtonWaldack <76004887+ColtonWaldack@users.noreply.github.com>
2021-03-24 22:07:55 -0400

    Update Palindrome.java
    Now recursive!

--- Palindrome.java
+++ Palindrome.java
@@ -2,57 +2,34 @@ import java.util.Scanner;
 public class Palindrome {
 
 	private boolean isPalindrome(String word) {
-		int x = 0;
-		int y = word.length() - 1;
-		while (x < y) {
-			if (word.charAt(x) != word.charAt(y)){
-        		return false;}
-        	x++;
-            y--;
-        }
-        return true;
-	}
-	/*
-	 * Open up  the file.
-	 * Break into words
-	 *  print out all Palindromes
-	 */
-	private void findAllPalindromes(String fileName) {
 
+		if(word.length() == 0 || word.length() == 1)
+			return true;
+		if (word.charAt(0) == word.charAt(word.length() - 1)){ // checks if two chars match
+			return isPalindrome(word.substring(1, word.length()-1)); // checks next two chars using substring and loops until length = 1 or 0
+		}
+
+		return false;
 
 	}
+
 	public Palindrome() {
 
-		Scanner testword = new Scanner(System.in);
+	Scanner testword = new Scanner(System.in);
         System.out.println("Enter a word to test");
         String word = testword.nextLine();
         testword.close();
         word = word.toLowerCase();
 
-
-
-
-
 		if (isPalindrome(word)) {
 		    System.out.println("Pass"); }
 		else {
-			 System.out.println("Fail"); }
+		    System.out.println("Fail"); }
 								}
 
 
-
-
-
-
-
-
 	public static void main(String[] args) {
-		new Palindrome();
 
-
-
-		System.out.println("I believe that this program has a Big O of  O(N) ");
-		System.out.println("This is because each letter of a word is checked at maximum once and compared to another letter");
 
 	}
 
